4919f9d655a4982433e71998d5779345f739cee6
stardiviner <numbchild@gmail.com>
2021-03-07 10:09:07 +0800

    Add demo code in core.clj

--- src/hello_seesaw/core.clj
+++ src/hello_seesaw/core.clj
@@ -1,6 +1,20 @@
-(ns hello-seesaw.core)
+(ns hello-seesaw.core
+  (:use seesaw.core)
+  (:require [sanersubstance.core :as substance])
+  (:import [org.pushingpixels.substance.api SubstanceLookAndFeel]
+           [org.pushingpixels.substance.api.skin GraphiteSkin]
+           [org.pushingpixels.substance.internal.utils SubstanceColorUtilities]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn set-theme!  []
+  (native!)
+  (SubstanceLookAndFeel/setSkin (GraphiteSkin.))
+  (substance/enforce-event-dispatch))
+
+(defn -main [& args]
+  (set-theme!)
+  (invoke-later
+   (-> (frame :title "Hello",
+              :content "Hello, Seesaw",
+              :on-close :dispose)
+       pack!
+       show!)))
