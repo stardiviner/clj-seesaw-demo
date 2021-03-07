ccb52a0a6ce5a723d0cf7365835df96a4b46b1c7
stardiviner <numbchild@gmail.com>
2021-03-07 10:08:24 +0800

    Add seesaw dependencies

--- project.clj
+++ project.clj
@@ -2,6 +2,10 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :repl-options {:init-ns hello-seesaw.core})
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [seesaw "1.5.0"]
+                 [sanersubstance "0.1.0-SNAPSHOT"]
+                 [com.formdev/flatlaf "0.41"]]
+  :repl-options {:init-ns hello-seesaw.core}
+  :jvm-options ["-Dsun.java2d.dpiaware=true" "-Dglass.gtk.uiScale=200%"])
